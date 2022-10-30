78f5df3701a5e88ece39c7d09b3dca0b683bf8a9
saxenanickk <saxenanickk@gmail.com>
2022-10-30 22:46:08 +0530

    Square rotation animation completed

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import { Interpolate } from "./src/Interpolate";
+import { PanGestureHandlerDemo } from "./src/PanGestureHandler";
+import { SquareRotation } from "./src/SquareRotation";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <SquareRotation /> */}
+      {/* <PanGestureHandlerDemo /> */}
+      <Interpolate />
     </View>
   );
 }
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
